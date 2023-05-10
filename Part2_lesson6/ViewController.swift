9e9f1cbbf6a02e46ae53caeda9a7271f8d5a3425
Daniil Chupin <danielchupin@yandex.ru>
2023-05-10 12:31:59 +0700

    Initial Commit

---
+++ Part2_lesson6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Part2_lesson6
+//
+//  Created by Даниил Чупин on 10.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
